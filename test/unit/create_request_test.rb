e8fbd94e09c123026edc933cfc99f00fdd4b74b5
Gert Findel <gert@acid.cl>
2011-06-23 19:04:44 -0400

    testing y request

---
+++ test/unit/create_request_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+
+class CreateRequestTest < Test::Unit::TestCase
+  
+  def setup 
+    @req = PuntoPagos::Request.new('test')
+  end
+  
+  def test_valid_create
+    puts "-------"
+    puts "webpay valid"
+    
+    data = { 
+  		'trx_id' 	=> "#{Time.now.to_i}",
+  		'monto'		=> '1000.00'
+  		}    
+  	resp = @req.create(data)
+    
+    assert resp.success?
+    
+#    payload = YAML.load_file(File.join(File.dirname(__FILE__),"..", "data","webpay_paylaod.yml"))
+  end
+  
+  def test_invalid_webpay_pay
+    puts "webpay invalid"
+    
+    assert resp.success? == false
+    
+  end
+
+end
